2bb72beb2a87353e8c8f152c2c47d07d10c71eca
Valeriu Tihai <valeriu@tihai.md>
2013-11-26 10:45:31 -0500

    validator

--- tp2/js/main.js
+++ tp2/js/main.js
@@ -2,3 +2,25 @@
 $(".carousel").carousel({
 	interval: 4000
 })
+
+// JavaScript validation
+/*
+		window.addEventListener("load", loadEvents, false);
+
+		function loadEvents() {
+			var eljs 	= document.getElementById("js");
+			var elhtml 	= document.getElementById("htmlcss");
+			var elphp 	= document.getElementById("php");
+			var eldes 	= document.getElementById("design");
+
+			eljs.addEventListener("change", addRangeValue, false);
+			elhtml.addEventListener("change", addRangeValue, false);
+			elphp.addEventListener("change", addRangeValue, false);
+			eldes.addEventListener("change", addRangeValue, false);
+		}
+
+		function addRangeValue(el) {
+			//console.log(el.target.previousElementSibling);
+			el.target.previousElementSibling.innerHTML = el.target.value;
+		}
+*/
